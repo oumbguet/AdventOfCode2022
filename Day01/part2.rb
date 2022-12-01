6c07b67b71dafdd15b7d2be3d8c051bb89e5fa71
florian millet <florian.millet@epitech.eu>
2022-12-01 10:13:29 +0100

    feat: day 01 gold star

---
+++ Day01/part2.rb
@@ -0,0 +1,19 @@
+file = File.open("input", "r")
+lines = file.readlines.map(&:chomp)
+
+arr = []
+
+total = 0
+
+lines.each do |line|
+    if line == ""
+        arr << total
+        total = 0
+    else
+        total += line.to_i
+    end
+end
+
+arr << total
+
+puts arr.sort()[-3..-1].sum()
